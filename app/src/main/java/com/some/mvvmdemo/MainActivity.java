afc54b1530d2795c8392cd2a60d3f617a14038c9
xiangxing <993819762@qq.com>
2020-02-09 17:47:02 +0800

    Initial commit

---
+++ app/src/main/java/com/some/mvvmdemo/MainActivity.java
@@ -0,0 +1,34 @@
+package com.some.mvvmdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.some.mvvmdemo.databinding.ActivityMainBinding;
+import com.some.mvvmdemo.entity.Account;
+
+public class MainActivity extends AppCompatActivity {
+
+    Account account;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this,
+                R.layout.activity_main);
+
+        account = new Account("XiaoLei",100);
+        binding.setAccount(account);
+        binding.setActivity(this);
+
+
+
+    }
+
+    public void onclick(View view){
+        Toast.makeText(this,"点击",Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/some/mvvmdemo/entity/Account.java
@@ -0,0 +1,29 @@
+package com.some.mvvmdemo.entity;
+
+public class Account {
+
+    private String name;
+
+    private int level;
+
+    public Account(String name, int level){
+        this.name = name;
+        this.level = level;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+}
