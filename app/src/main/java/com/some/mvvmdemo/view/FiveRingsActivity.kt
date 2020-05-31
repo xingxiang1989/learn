1f235862d9d48bae820b233267fdd19abdac7b68
xiangxing <993819762@qq.com>
2020-05-31 10:37:53 +0800

    five ring

--- app/src/main/java/com/some/mvvmdemo/view/FiveRingsActivity.kt
+++ app/src/main/java/com/some/mvvmdemo/view/FiveRingsActivity.kt
@@ -4,29 +4,17 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.some.mvvmdemo.R
 import com.some.mvvmdemo.base.BaseActiviy
-import com.some.mvvmdemo.databinding.ActivityViewgroupBinding
+import com.some.mvvmdemo.databinding.ActivityFiveRingBinding
 
 /**
  * @author xiangxing
  */
 class FiveRingsActivity: BaseActiviy() {
 
-    lateinit  var binding: ActivityViewgroupBinding
+    lateinit  var binding: ActivityFiveRingBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_viewgroup)
-
-//        var degree = 0
-//        Observable.interval(100,TimeUnit.MILLISECONDS)
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .subscribe { Consumer<Long> {
-//                        LogUtils.d("1111")
-//                        degree += 10
-//                        binding.roateRing.setDegreeAngele(degree)
-//                    }
-//                    io.reactivex.functions.Consumer<Throwable> { t -> LogUtils.d("t=${t.toString()}") }
-//                }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_five_ring)
     }
 }
