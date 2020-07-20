9995dc73bf44aa686d252348c2fc63399576d638
xiangxing <993819762@qq.com>
2020-07-20 16:48:38 +0800

    区分Manifest.xml文件，增加application，针对被引用的库，增加判断

---
+++ modulea/src/main/java/debug/MainApplication.java
@@ -0,0 +1,14 @@
+package debug;
+
+import android.app.Application;
+
+/**
+ * @author xiangxing
+ */
+public class MainApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
