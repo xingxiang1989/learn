225d344dd66d5396b6d5fdc90d8f6081456d63bb
xiangxing <993819762@qq.com>
2020-07-22 21:25:53 +0800

    动画

--- modulea/src/main/java/com/some/modulea/ModuleAActivity.java
+++ modulea/src/main/java/com/some/modulea/ModuleAActivity.java
@@ -1,6 +1,9 @@
 package com.some.modulea;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
@@ -10,9 +13,21 @@ import com.alibaba.android.arouter.facade.annotation.Route;
 @Route(path = "/moduleA/moduleA")
 public class ModuleAActivity extends FragmentActivity {
 
+    LinearLayout ll;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_modulea);
+
+        ll = findViewById(R.id.root);
+
+        findViewById(R.id.add).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView item = new TextView(ModuleAActivity.this);
+                item.setText("moduleA");
+                ll.addView(item,0);
+            }
+        });
     }
 }
