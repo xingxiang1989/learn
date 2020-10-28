380c811ffa9c6f459c15e14fbd239ee5e12fa091
xiangxing <993819762@qq.com>
2020-10-29 00:23:16 +0800

    jiagu 创建task，准备进行加固

--- jiaGuPlugin/src/main/java/com/some/jiaguplugin/JiaGuPlugin.java
+++ jiaGuPlugin/src/main/java/com/some/jiaguplugin/JiaGuPlugin.java
@@ -1,8 +1,14 @@
 package com.some.jiaguplugin;
 
+import com.android.build.gradle.AppExtension;
+import com.android.build.gradle.api.ApplicationVariant;
+import com.android.build.gradle.api.BaseVariantOutput;
+
 import org.gradle.api.Action;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+
+import java.io.File;
 
 /**
  * 插件也需要注册
@@ -13,13 +19,31 @@ public class JiaGuPlugin implements Plugin<Project> {
     public void apply(Project project) {
         //因为需要用户填数据，因此要使用插件扩展
         //让使用者配置JiaguExt的参数
-        final JiaguExt jiagu = project.getExtensions().create("jiagu",JiaguExt.class);
+        final JiaguExt jiaguExt = project.getExtensions().create("jiagu",JiaguExt.class);
 
         //注册监听
         project.afterEvaluate(new Action<Project>() {
             @Override
-            public void execute(Project project) {
-                System.out.println(jiagu.getUserName());
+            public void execute(final Project project) {
+
+                AppExtension appExtension =
+                        project.getExtensions().getByType(AppExtension.class);
+
+                //得到一个集合，【debug， Release】
+                appExtension.getApplicationVariants().all(new Action<ApplicationVariant>() {
+                    @Override
+                    public void execute(ApplicationVariant applicationVariant) {
+                        applicationVariant.getOutputs().all(new Action<BaseVariantOutput>() {
+                            @Override
+                            public void execute(BaseVariantOutput baseVariantOutput) {
+                                File outPutFile = baseVariantOutput.getOutputFile();
+                                String name = baseVariantOutput.getName();
+                                project.getTasks().create("jiagu" + name,
+                                        JiaguTask.class, outPutFile, jiaguExt);
+                            }
+                        });
+                    }
+                });
             }
         });
     }
---
+++ jiaGuPlugin/src/main/java/com/some/jiaguplugin/JiaguTask.java
@@ -0,0 +1,40 @@
+package com.some.jiaguplugin;
+
+import org.gradle.api.Action;
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+import org.gradle.process.ExecSpec;
+
+import java.io.File;
+
+import javax.inject.Inject;
+
+/**
+ * @author xiangxing
+ */
+public class JiaguTask extends DefaultTask {
+
+    File apkFile;
+    JiaguExt jiaguExt;
+
+    /**
+     * 用于设置分组，比如jiagudebug，jiagurelease 就会放在jiagu文件夹里
+     */
+    @Inject
+    public JiaguTask(File apkFile, JiaguExt jiaguExt){
+        setGroup("jiagu");
+        this.apkFile = apkFile;
+        this.jiaguExt = jiaguExt;
+    }
+
+    @TaskAction
+    public void abs(){
+        //完成加固
+        getProject().exec(new Action<ExecSpec>() {
+            @Override
+            public void execute(ExecSpec execSpec) {
+                execSpec.commandLine("java","-version");
+            }
+        });
+    }
+}
