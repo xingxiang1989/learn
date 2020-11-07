9bd864cfd411f7dab0a3a56e30513ea0084e1b90
xiangxing <993819762@qq.com>
2020-11-07 10:25:51 +0800

    软引用

--- app/src/main/java/com/some/mvvmdemo/reflect/ReflectActivity.java
+++ app/src/main/java/com/some/mvvmdemo/reflect/ReflectActivity.java
@@ -9,8 +9,11 @@ import com.some.mvvmdemo.R;
 import com.some.mvvmdemo.base.BaseActiviy;
 import com.some.mvvmdemo.entity.Account;
 
+import java.lang.ref.SoftReference;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author xiangxing
@@ -22,19 +25,43 @@ public class ReflectActivity extends BaseActiviy {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reflect);
 
+//        try {
+//            Class classObj = Class.forName("com.some.mvvmdemo.entity.Account");
+//            Constructor constructor = classObj.getConstructor(String.class,int.class);
+//            //强制性转换用于演示
+//            String log = ((Account)constructor.newInstance("111",222)).toString();
+//            LogUtils.d("log = " + log);
+//
+//            Object object = constructor.newInstance("111",222);
+//
+//            classObj.getMethod("setLevel",new Class[]{int.class}).invoke(object,102);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+        byte[] cacheData = new byte[100 * 1024 * 1024];
+        //软引用，1.没有强引用，2.内存不足才会回收
+        SoftReference<byte[]> softReference = new SoftReference<>(cacheData);
+        cacheData = null;
+
+        LogUtils.d("gc前 cacheData = " + cacheData);
+        LogUtils.d("gc前 softReference.get() = " + softReference.get());
+
+        System.gc();
+
+
         try {
-            Class classObj = Class.forName("com.some.mvvmdemo.entity.Account");
-            Constructor constructor = classObj.getConstructor(String.class,int.class);
-            //强制性转换用于演示
-            String log = ((Account)constructor.newInstance("111",222)).toString();
-            LogUtils.d("log = " + log);
-
-            Object object = constructor.newInstance("111",222);
-
-            classObj.getMethod("setLevel",new Class[]{int.class}).invoke(object,102);
-        } catch (Exception e) {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        LogUtils.d("gc后 cacheData = " + cacheData);
+        LogUtils.d("gc后 softReference.get() = " + softReference.get());
+
+        byte[] allcote = new byte[400 * 1024 * 1024];
+
+        LogUtils.d("分配内存后， cacheData = " + cacheData);
+        LogUtils.d("分配内存后， softReference.get() = " + softReference.get());
 
     }
 }
