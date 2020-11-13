1ab3b35f502c89974eb413180e4189e340e5a509
xiangxing <993819762@qq.com>
2020-11-13 17:07:00 +0800

    换肤

--- app/src/main/java/com/some/mvvmdemo/reflect/ReflectActivity.java
+++ app/src/main/java/com/some/mvvmdemo/reflect/ReflectActivity.java
@@ -1,8 +1,16 @@
 package com.some.mvvmdemo.reflect;
 
+import android.content.Context;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 
@@ -25,26 +33,113 @@ import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 import dalvik.system.DexClassLoader;
-import retrofit2.http.FieldMap;
 
 /**
  * @author xiangxing
  */
-public class ReflectActivity extends BaseActiviy {
+public class ReflectActivity extends BaseActiviy implements View.OnClickListener {
 
     private static final String EXTRACTED_NAME_EXT = ".classes";
+    private ImageView imageView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reflect);
 
+        imageView = findViewById(R.id.imageView);
+        findViewById(R.id.btn1).setOnClickListener(this);
+        findViewById(R.id.btn2).setOnClickListener(this);
+
+
 //        addPluginApkToDex("");
 
 //        testInnovationHandler();
 
-        testClassLoaderFromApk();
+//        testClassLoaderFromApk();
     }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn1){
+            switchSkin(1);
+        }else if(v.getId() == R.id.btn2){
+            switchSkin(2);
+        }
+    }
+
+    /**
+     * 切换皮肤
+     * @param type
+     */
+    public void switchSkin(int type) {
+        String skinType;
+        if(type == 1){
+            skinType= "app-test.apk";
+        }else {
+            skinType= "children.apk";
+        }
+        final String path = Environment.getExternalStorageDirectory() + File.separator + skinType;
+        final String pkgName = getUninstallApkPkgName(this, path);
+
+        LogUtils.d("switchSkin pkgName =" + pkgName);
+        dynamicLoadApk(path,pkgName);
+    }
+
+
+    /**
+     * 获取未安装apk的信息
+     * @param context
+     * @param pApkFilePath apk文件的path
+     * @return
+     */
+    private String getUninstallApkPkgName(Context context, String pApkFilePath) {
+        PackageManager pm = context.getPackageManager();
+        PackageInfo pkgInfo = pm.getPackageArchiveInfo(pApkFilePath, PackageManager.GET_ACTIVITIES);
+        if (pkgInfo != null) {
+            ApplicationInfo appInfo = pkgInfo.applicationInfo;
+            return appInfo.packageName;
+        }
+        return "";
+    }
+
+    /**
+     *
+     * @param pApkFilePath
+     * @param pApkPacketName
+     */
+    private  void dynamicLoadApk(String pApkFilePath,String pApkPacketName){
+
+        LogUtils.d("dynamicLoadApk start pApkPacketName =" + pApkPacketName);
+
+        File file=getDir("dex", Context.MODE_PRIVATE);
+        //第一个参数：是dex压缩文件的路径
+        //第二个参数：是dex解压缩后存放的目录
+        //第三个参数：是C/C++依赖的本地库文件目录,可以为null
+        //第四个参数：是上一级的类加载器
+        DexClassLoader  classLoader=new DexClassLoader(pApkFilePath,file.getAbsolutePath(),null,getClassLoader());
+        try {
+            final Class<?> loadClazz = classLoader.loadClass(pApkPacketName +
+                    ".R$mipmap");
+            //插件中皮肤的名称是skin_one
+            final Field skinOneField = loadClazz.getDeclaredField("skin_one");
+            skinOneField.setAccessible(true);
+            //反射获取skin_one的resousreId
+            final int resousreId = (int) skinOneField.get(R.id.class);
+            //可以加载插件资源的Resources
+            final Resources resources = createResources(pApkFilePath);
+            if (resources != null) {
+                final Drawable drawable = resources.getDrawable(resousreId);
+                imageView.setBackground(drawable);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            LogUtils.e("dynamicLoadApk e =" + e.toString());
+        }
+
+    }
+
 
     /**
      * 插件化将apk中的dex文件加入进来
@@ -256,5 +351,38 @@ public class ReflectActivity extends BaseActiviy {
             LogUtils.d("testClassLoaderFromApk Exception e= " + e.toString());
 
         }
+    }
+
+    /**
+     * 把插件文件设置进去，生成AssetManager
+     * @param pFilePath
+     */
+    private AssetManager createAssetManager(String pFilePath){
+
+        try {
+            AssetManager assetManager = AssetManager.class.newInstance();
+            Class classzz = Class.forName("android.content.res.AssetManager");
+            Method method = classzz.getDeclaredMethod("addAssetPath",String.class);
+            method.setAccessible(true);
+            method.invoke(assetManager, pFilePath);
+            return assetManager;
+
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * 这个Resources就可以加载非宿主apk中的资源
+     * @param pFilePath
+     * @return
+     */
+    private Resources createResources(String pFilePath){
+        final AssetManager assetManager = createAssetManager(pFilePath);
+        Resources superRes = this.getResources();
+        return new Resources(assetManager, superRes.getDisplayMetrics(), superRes.getConfiguration());
     }
 }
