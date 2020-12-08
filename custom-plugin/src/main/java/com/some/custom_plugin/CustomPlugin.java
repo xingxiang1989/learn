04b338f7442de4f90311e33328b118e142809613
xiangxing <993819762@qq.com>
2020-12-08 10:12:26 +0800

    cutstomPlugin asm 代码添加

--- custom-plugin/src/main/java/com/some/custom_plugin/CustomPlugin.java
+++ custom-plugin/src/main/java/com/some/custom_plugin/CustomPlugin.java
@@ -1,6 +1,7 @@
 package com.some.custom_plugin;
 
 import com.android.build.gradle.AppExtension;
+import com.some.custom.PageTransform;
 
 import org.gradle.api.Action;
 import org.gradle.api.Plugin;
@@ -13,7 +14,7 @@ public class CustomPlugin implements Plugin<Project> {
         System.out.println("CustomPlugin 执行了");
 
         AppExtension appExtension = project.getExtensions().getByType(AppExtension.class);
-        appExtension.registerTransform(new AsmTransform2());
+//        appExtension.registerTransform(new AsmTransform2());
 
         project.beforeEvaluate(new Action<Project>() {
             @Override
