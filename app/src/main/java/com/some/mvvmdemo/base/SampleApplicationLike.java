93297b34cd67a5055700f0c6733c0c0cc4e35006
xiangxing <993819762@qq.com>
2020-12-15 14:07:34 +0800

    tinker 从官方demo中copy 一些代码过来

---
+++ app/src/main/java/com/some/mvvmdemo/base/SampleApplicationLike.java
@@ -0,0 +1,84 @@
+package com.some.mvvmdemo.base;
+
+import android.annotation.TargetApi;
+import android.app.Application;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.multidex.MultiDex;
+
+import com.some.common.Log.MyLogImp;
+import com.some.common.util.SampleApplicationContext;
+import com.some.common.util.TinkerManager;
+import com.tencent.tinker.anno.DefaultLifeCycle;
+import com.tencent.tinker.entry.DefaultApplicationLike;
+import com.tencent.tinker.lib.tinker.Tinker;
+import com.tencent.tinker.lib.tinker.TinkerInstaller;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
+
+/**
+ * @author xiangxing
+ */
+@DefaultLifeCycle(application = "tinker.sample.android.app.MainAppApplication",
+        flags = ShareConstants.TINKER_ENABLE_ALL,
+        loadVerifyFlag = false)
+public class SampleApplicationLike extends DefaultApplicationLike {
+
+    private static final String TAG = "Tinker.SampleApplicationLike";
+
+    public SampleApplicationLike(Application application, int tinkerFlags, boolean tinkerLoadVerifyFlag,
+                                 long applicationStartElapsedTime, long applicationStartMillisTime, Intent tinkerResultIntent) {
+        super(application, tinkerFlags, tinkerLoadVerifyFlag, applicationStartElapsedTime, applicationStartMillisTime, tinkerResultIntent);
+    }
+
+    /**
+     * install multiDex before install tinker
+     * so we don't need to put the tinker lib classes in the main dex
+     *
+     * @param base
+     */
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    @Override
+    public void onBaseContextAttached(Context base) {
+        super.onBaseContextAttached(base);
+        //you must install multiDex whatever tinker is installed!
+        MultiDex.install(base);
+
+        SampleApplicationContext.application = getApplication();
+        SampleApplicationContext.context = getApplication();
+        TinkerManager.setTinkerApplicationLike(this);
+
+        TinkerManager.initFastCrashProtect();
+        //should set before tinker is installed
+        TinkerManager.setUpgradeRetryEnable(true);
+
+        //optional set logIml, or you can use default debug log
+        TinkerInstaller.setLogIml(new MyLogImp());
+
+        //installTinker after load multiDex
+        //or you can put com.tencent.tinker.** to main dex
+        TinkerManager.installTinker(this);
+        Tinker tinker = Tinker.with(getApplication());
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    public void registerActivityLifecycleCallbacks(Application.ActivityLifecycleCallbacks callback) {
+        getApplication().registerActivityLifecycleCallbacks(callback);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        init();
+
+    }
+
+    private void init(){
+
+    }
+
+    private void onUserEvent(){
+
+    }
+}
--- common/src/main/java/com/some/common/Utils.java
+++
@@ -1,8 +0,0 @@
-package com.some.common;
-
-public class Utils {
-
-    public static String getString(){
-        return "1111";
-    }
-}
