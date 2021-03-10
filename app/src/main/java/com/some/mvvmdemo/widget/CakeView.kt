5d7555fb2613ca7f62e2aabe4430570f02c816ba
xiangxing <993819762@qq.com>
2021-03-10 15:58:19 +0800

    在扇形上绘制文字

--- app/src/main/java/com/some/mvvmdemo/widget/CakeView.kt
+++ app/src/main/java/com/some/mvvmdemo/widget/CakeView.kt
@@ -7,12 +7,11 @@ import android.graphics.Paint
 import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.blankj.utilcode.util.LogUtils
 import com.blankj.utilcode.util.SizeUtils
-import com.blankj.utilcode.util.SpanUtils
 import com.some.mvvmdemo.R
 import com.some.mvvmdemo.entity.CakeBean
-import com.some.mvvmdemo.testkotlin.b
 
 /**
  * @author xiangxing
@@ -24,17 +23,22 @@ class CakeView: View {
     var mList: MutableList<CakeBean>?= null
     var minParamer: Int =0
     var sum = 0f
+    var mTextPaint: Paint
 
     var rectW = SizeUtils.dp2px(40f)
     var rectH = SizeUtils.dp2px(25f)
 
-    constructor(context: Context?) : this(context,null)
-    constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs,0)
-    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super
-    (context, attrs, defStyleAttr){
+    constructor(context: Context?) : this(context, null)
+    constructor(context: Context?, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr){
 
         mPaint = Paint()
         mPaint.isAntiAlias = true
+
+        mTextPaint = Paint()
+        mTextPaint.isAntiAlias = true
+        mTextPaint.color = Color.BLACK
+        mTextPaint.textSize = 20f
 
     }
 
@@ -112,7 +116,7 @@ class CakeView: View {
         }
 
         LogUtils.d("onMeasure width = $width , height= $height")
-        setMeasuredDimension(width,height)
+        setMeasuredDimension(width, height)
 
     }
 
@@ -120,7 +124,7 @@ class CakeView: View {
         super.onSizeChanged(w, h, oldw, oldh)
         LogUtils.d("onSizeChanged w=$w,h=$h,oldw=$oldh,oldH=$oldh")
         LogUtils.d("onSizeChanged measuredHeight=$measuredHeight,measuredWidth=$measuredWidth")
-        minParamer = Math.min(measuredHeight,measuredWidth)
+        minParamer = Math.min(measuredHeight, measuredWidth)
 
     }
 
@@ -132,29 +136,71 @@ class CakeView: View {
             return
         }
         canvas?.drawColor(Color.YELLOW)
-        var rectF = RectF(0f,0f,minParamer.toFloat(),minParamer.toFloat())
+        var rectF = RectF(0f, 0f, minParamer.toFloat(), minParamer.toFloat())
         var startDegree = 0f
 
-        var left = (minParamer + SizeUtils.dp2px(50f)).toFloat()
+        var left = (minParamer + SizeUtils.dp2px(10f)).toFloat()
         var right = left + rectW
         var top = 0f
+
+        LogUtils.d("minParamer = $minParamer, left = $left, right = $right")
+
 
         for(bean in mList!!){
 
             mPaint.color = bean.color
-            canvas?.drawArc(rectF,startDegree,bean.degree,true,mPaint)
+            //绘制扇形区域
+            canvas?.drawArc(rectF, startDegree, bean.degree, true, mPaint)
+
+            drawText2(startDegree,bean,canvas)
+
             startDegree += bean.degree
 
-            canvas?.drawRect(left,top,right,top+rectH,mPaint)
-
+            canvas?.drawRect(left, top, right, top + rectH, mPaint)
             //设置文字
             mPaint.color = Color.BLACK
             mPaint.textSize = 30f
-            canvas?.drawText(bean.name + bean.amount + "%", right + SizeUtils.dp2px(10f)
-                    ,top + SizeUtils.dp2px(15f),
+            canvas?.drawText(bean.name + bean.amount + "%", right + SizeUtils.dp2px(10f), top + SizeUtils.dp2px(15f),
                     mPaint)
 
             top+= rectH
         }
+    }
+
+    /**
+     * 方法1：绘制扇形上文字,这种方式不合适，出现的问题是文字方向永远是横着的
+     * 效果不太好，可以弃用
+     */
+    private fun drawText(startDegree: Float, bean: CakeBean, canvas: Canvas?) {
+        //计算文字位置角度
+        val textAngle: Float = startDegree + bean.degree / 2
+        val x = minParamer / 2 + (minParamer / 4 * Math.cos(textAngle * Math.PI / 180))
+                .toFloat()
+        //计算文字位置坐标
+        val y = minParamer / 2 + (minParamer / 4 * Math.sin(textAngle * Math.PI / 180))
+                .toFloat()
+        canvas?.drawText(bean.name ?: "", x, y, mTextPaint)
+
+        LogUtils.d("name = ${bean.name}, textAngle = $textAngle, x= $x, y= $y")
+    }
+
+    /**
+     * 旋转画布，
+     */
+    private fun drawText2(startDegree: Float, bean: CakeBean, canvas: Canvas?) {
+        //计算文字位置角度
+        val textAngle: Float = startDegree + bean.degree / 2
+        val x = (minParamer/2.0).toFloat() + SizeUtils.dp2px(50f)
+        val y = (minParamer/2.0).toFloat()
+
+        val centerX = (minParamer/2.0).toFloat()
+        val centerY = (minParamer/2.0).toFloat()
+
+        canvas?.rotate(textAngle,centerX,centerY)
+
+        canvas?.drawText(bean.name ?: "",x , y, mTextPaint)
+        canvas?.rotate(-textAngle,centerX,centerY)
+
+        LogUtils.d("name = ${bean.name}, textAngle = $textAngle, x= $x, y= $y")
     }
 }
