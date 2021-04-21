77006d551d95fa30bda37a6b4e12eb582eb54b62
xiangxing <993819762@qq.com>
2021-04-21 19:48:57 +0800

    通过mFactorySet反射获取已经不行了，会闪退。直接反射拿到factory变量，然后设置进去

--- changeSkin/src/main/java/com/some/changeskin/ApplicationActivityLifecycle.kt
+++ changeSkin/src/main/java/com/some/changeskin/ApplicationActivityLifecycle.kt
@@ -3,6 +3,7 @@ package com.some.changeskin
 import android.app.Activity
 import android.app.Application
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import androidx.collection.ArrayMap
 import androidx.core.view.LayoutInflaterCompat
@@ -14,6 +15,7 @@ import java.util.*
  */
 class ApplicationActivityLifecycle(observable: Observable): Application.ActivityLifecycleCallbacks {
 
+    private val tag ="ApplicationActivityLifecycle"
     var mObservable : Observable?= null
     /**
      * 存储每个activity对应的LayoutInflater.Factory2
@@ -25,6 +27,8 @@ class ApplicationActivityLifecycle(observable: Observable): Application.Activity
     }
 
     override fun onActivityCreated(activity: Activity, p1: Bundle?) {
+
+        Log.d(tag,"onActivityCreated, activity = ${activity.javaClass.name}")
         /**
          * TODO：需要查看onActivityCreated执行时机，factory2是在何时进行设置的
          * 更新状态栏
@@ -32,13 +36,27 @@ class ApplicationActivityLifecycle(observable: Observable): Application.Activity
         SkinThemeUtils.updateStatusBarColor(activity)
         val layoutInflater = activity.layoutInflater
 
-        val field = LayoutInflater::class.java.getDeclaredField("mFactorySet")
-        field.isAccessible = true
-        field.setBoolean(layoutInflater,false)
-
-        //使用factory2 设置布局加载工程
         val skinLayoutInflaterFactory = SkinLayoutInflaterFactory(activity)
-        LayoutInflaterCompat.setFactory2(layoutInflater,skinLayoutInflaterFactory)
+
+        /**
+         * 通过mFactorySet反射获取已经不行了，会闪退。直接反射拿到factory变量，然后设置进去
+         */
+//        val field = LayoutInflater::class.java.getDeclaredField("mFactorySet")
+//        field.isAccessible = true
+//        field.setBoolean(layoutInflater,false)
+//        //使用factory2 设置布局加载工程
+//        LayoutInflaterCompat.setFactory2(layoutInflater,skinLayoutInflaterFactory)
+
+        val fieldFactory2 = LayoutInflater::class.java.getDeclaredField("mFactory2")
+        fieldFactory2.isAccessible = true
+        val fieldFactory = LayoutInflater::class.java.getDeclaredField("mFactory")
+        fieldFactory.isAccessible = true
+        fieldFactory2.set(layoutInflater,skinLayoutInflaterFactory)
+        fieldFactory.set(layoutInflater,skinLayoutInflaterFactory)
+
+
+
+
         mLayoutInflaterFactories[activity] = skinLayoutInflaterFactory
 
         //每一个页面会创建，那么就会作为观察者被加入进去
